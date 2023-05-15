dc8096123169f6ac26af5123071098a7a294a826
Yoon TaeKeun <ytg7146@snu.ac.kr>
2023-05-15 16:31:08 +0900

    organizing folder

---
+++ LSTMcuda/main.py
@@ -0,0 +1,115 @@
+# reference : https://cnvrg.io/pytorch-lstm/
+
+import numpy as np
+import pandas as pd
+import torch #pytorch
+
+from torch.autograd import Variable 
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+from model.LSTM import LSTM1
+
+import torch #pytorch
+
+import matplotlib.pyplot as plt
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+
+df = pd.read_csv('./LSTMcuda/SBUX.csv', index_col = 'Date', parse_dates=True)
+
+
+plt.style.use('ggplot')
+df['Volume'].plot(label='CLOSE', title='Star Bucks Stock Volume')
+plt.savefig('./LSTMcuda/SBUX_volume.png')
+
+X = df.iloc[:, :-1]
+y = df.iloc[:, 5:6] 
+
+
+mm = MinMaxScaler()
+ss = StandardScaler()
+
+
+X_ss = ss.fit_transform(X)
+y_mm = mm.fit_transform(y) 
+
+
+#first 200 for training
+
+X_train = X_ss[:200, :]
+X_test = X_ss[200:, :]
+
+y_train = y_mm[:200, :]
+y_test = y_mm[200:, :] 
+
+print("Training Shape", X_train.shape, y_train.shape)
+print("Testing Shape", X_test.shape, y_test.shape) 
+
+X_train_tensors = Variable(torch.Tensor(X_train)).to(device)
+X_test_tensors = Variable(torch.Tensor(X_test)).to(device)
+
+y_train_tensors = Variable(torch.Tensor(y_train)).to(device)
+y_test_tensors = Variable(torch.Tensor(y_test)).to(device)
+
+#reshaping to rows, timestamps, features
+
+X_train_tensors_final = torch.reshape(X_train_tensors,   (X_train_tensors.shape[0], 1, X_train_tensors.shape[1]))
+
+
+X_test_tensors_final = torch.reshape(X_test_tensors,  (X_test_tensors.shape[0], 1, X_test_tensors.shape[1])) 
+
+print("Training Shape", X_train_tensors_final.shape, y_train_tensors.shape)
+print("Testing Shape", X_test_tensors_final.shape, y_test_tensors.shape) 
+
+num_epochs = 1000 #1000 epochs
+learning_rate = 0.001 #0.001 lr
+
+input_size = 5 #number of features
+hidden_size = 2 #number of features in hidden state
+num_layers = 1 #number of stacked lstm layers
+
+num_classes = 1 #number of output classes 
+
+lstm1 = LSTM1(num_classes, input_size, hidden_size, num_layers, X_train_tensors_final.shape[1]).to(device) #our lstm class
+
+
+criterion = torch.nn.MSELoss()    # mean-squared error for regression
+optimizer = torch.optim.Adam(lstm1.parameters(), lr=learning_rate) 
+
+for epoch in range(num_epochs):
+  outputs = lstm1.forward(X_train_tensors_final) #forward pass
+  optimizer.zero_grad() #caluclate the gradient, manually setting to 0
+ 
+  # obtain the loss function
+  loss = criterion(outputs, y_train_tensors)
+ 
+  loss.backward() #calculates the loss of the loss function
+ 
+  optimizer.step() #improve from loss, i.e backprop
+  if epoch % 100 == 0:
+    print("Epoch: %d, loss: %1.5f" % (epoch, loss.item())) 
+
+df_X_ss = ss.transform(df.iloc[:, :-1]) #old transformers
+df_y_mm = mm.transform(df.iloc[:, -1:]) #old transformers
+
+df_X_ss = Variable(torch.Tensor(df_X_ss)) #converting to Tensors
+df_y_mm = Variable(torch.Tensor(df_y_mm))
+#reshaping the dataset
+df_X_ss = torch.reshape(df_X_ss, (df_X_ss.shape[0], 1, df_X_ss.shape[1])) 
+
+train_predict = lstm1(df_X_ss.cuda()).cpu()#forward pass
+data_predict = train_predict.data.numpy() #numpy conversion
+dataY_plot = df_y_mm.data.numpy()
+
+data_predict = mm.inverse_transform(data_predict) #reverse transformation
+dataY_plot = mm.inverse_transform(dataY_plot)
+plt.figure(figsize=(10,6)) #plotting
+plt.axvline(x=200, c='r', linestyle='--') #size of the training set
+
+plt.plot(dataY_plot, label='Actuall Data') #actual plot
+plt.plot(data_predict, label='Predicted Data') #predicted plot
+plt.title('SBUX volume Prediction')
+plt.legend()
+plt.savefig('./LSTMcuda/SBUX_volume_Prediction.png')
